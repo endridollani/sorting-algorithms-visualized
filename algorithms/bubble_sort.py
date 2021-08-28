ad58c8ac09d5be2c5c595e7989909f117f3425e7
Endri Dollani <44905521+endridollani@users.noreply.github.com>
2021-08-28 13:18:05 +0200

    Updated and refactored bubble_sort and rectangle class

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -5,19 +5,32 @@ class BubbleSort:
 	def __init__(self,window,rectangles):
 		self.window = window
 		self.rectangles = rectangles
+		self.swaped = False
 		self.array_of_numbers = self.rectangles.get_array_of_numbers()
 	
 	def display(self,window):
-		for i in range(len(self.array_of_numbers)):
-			for j in range(len(self.array_of_numbers) - 1):
+		array_size = len(self.array_of_numbers)
+		iterations = len(self.array_of_numbers) - 1
+		for i in range(array_size):
+			for j in range(iterations):
 				if self.array_of_numbers[j] > self.array_of_numbers[j+1]:
-					time.sleep(0.005)
-					self.swap(j)
-				window.set_bg()
-				self.rectangles.draw_rectangles(window.get_window())
-				pygame.display.flip()
+					time.sleep(0.0005)
+					self.swap(j,window)
+				
+				if not self.swaped:
+					window.set_bg()
+					self.rectangles.draw_rectangles(window.get_window())
+					pygame.display.flip()
+				self.swaped = False
+			array_size -=1
+			iterations -=1
+		self.rectangles.sort_finished(window.get_window())
+
 
 	
-	def swap(self, index):
-		self.array_of_numbers[index],self.array_of_numbers[index+1] = self.array_of_numbers[index+1],self.array_of_numbers[index]				
-
+	def swap(self, index,window):
+		self.swaped = True
+		self.array_of_numbers[index],self.array_of_numbers[index+1] = self.array_of_numbers[index+1],self.array_of_numbers[index]
+		window.set_bg()
+		self.rectangles.draw_rectangles(window=window.get_window(),swap1=index,swap2=index+1)
+		pygame.display.flip()
--- display/rectangles.py
+++ display/rectangles.py
@@ -3,6 +3,7 @@ import time
 import random
 pygame.init()
 
+
 class Rectangles():
     def __init__(self,number_of_rectangles):
         self.number_of_rectangles = number_of_rectangles
@@ -19,26 +20,31 @@ class Rectangles():
             left_p = round(1300 / self.number_of_rectangles) * pos
         return pygame.Rect(left_p,700 - round(650 * (val + 1) / (self.number_of_rectangles + 5)), round(1300 / self.number_of_rectangles),round(650 * (val + 1) / self.number_of_rectangles))
 
-    def draw_rectangles(self,window,swaped_colour = (255,0,0),sw1 = None,sw2 = None,finished = None):
+    def draw_rectangles(self,window,swaped_colour = (255,0,0),swap1 = None,swap2 = None):
 
         for i in self.array_of_numbers:
             val = i
             pos = self.array_of_numbers.index(val)
             rectangle = self.get_rectangle_at(pos,val)
             
-            if sw1 == pos:
+            if swap1 == pos:
                 pygame.draw.rect(window,swaped_colour, rectangle)
-                sw1 = None
-            elif sw2 == pos:
+                swap1 = None
+            elif swap2 == pos:
                 pygame.draw.rect(window,swaped_colour, rectangle)
-                sw2 = None
-            elif finished != None:
-                time.wait(50)
-                pygame.draw.rect(window,finished, rectangle)
-                pygame.display.flip()
-                self.default_color = finished
+                swap2 = None
             else:
                 pygame.draw.rect(window,self.default_color, rectangle)
+            
+    def sort_finished(self,window):
+        for i in self.array_of_numbers:
+            val = i
+            pos = self.array_of_numbers.index(val)
+            rectangle = self.get_rectangle_at(pos,val)
+
+            time.sleep(0.02)
+            pygame.draw.rect(window,(0, 176, 255), rectangle)
+            pygame.display.flip()
     
     def initialize_new_array_of_numbers(self):
         array_of_numbers = [n for n in range(self.number_of_rectangles)]
