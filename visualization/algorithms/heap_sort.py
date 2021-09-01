3a6c160f3d583c11722bc6fa6e10dbd076a28506
Endri Dollani <44905521+endridollani@users.noreply.github.com>
2021-09-01 21:16:11 +0200

    Heap sort implemented and visualized

--- visualization/algorithms/heap_sort.py
+++ visualization/algorithms/heap_sort.py
@@ -43,16 +43,66 @@ class HeapSort:
         display_sort_algorithm_information(self.window.get_window(
         ), "Heap Sort", self.comparisons, self.array_accesses, self.rectangles.number_of_rectangles, self.delay)
 
-    def draw_rectangles(self, start, end, pivot):
+    def draw_rectangles(self,index, largest):
         self.window.set_bg()
         self.rectangles.draw_heap_sort(
-            self.window.get_window(), start, end, pivot)
+            self.window.get_window(), index, largest)
         display_sort_algorithm_information(self.window.get_window(
         ), "Heap Sort", self.comparisons, self.array_accesses, self.rectangles.number_of_rectangles, self.delay)
         pygame.display.flip()
 
     def display(self, window):
         self.window = window
-        self.heap_sort()
+        self.heap_sort(self.array_of_numbers)
         self.rectangles.draw_rectangles(window.get_window())
         self.rectangles.sort_finished(self.window.get_window(), 0.01)
+
+    # Python program for implementation of heap Sort
+    
+    # To heapify subtree rooted at index i.
+    # n is size of heap
+    
+    def heapify(self,array, n, i):
+        largest = i  # Initialize largest as root
+        l = 2 * i + 1     # left = 2*i + 1
+        r = 2 * i + 2     # right = 2*i + 2
+    
+        # See if left child of root exists and is
+        # greater than root
+        if l < n and array[largest] < array[l]:
+            largest = l
+
+            self.array_accesses += 1
+            time.sleep(self.delay)
+            self.draw_rectangles(l,largest)
+    
+        # See if right child of root exists and is
+        # greater than root
+        if r < n and array[largest] < array[r]:
+            largest = r
+
+            time.sleep(self.delay)
+            self.draw_rectangles(r,largest)
+    
+        # Change root, if needed
+        if largest != i:
+            array[i], array[largest] = array[largest], array[i]  # swap
+           
+            time.sleep(self.delay)
+            self.draw_rectangles(i,largest)
+            # Heapify the root.
+            self.heapify(array, n, largest)
+
+    # The main function to sort an array of given size
+    def heap_sort(self,array):
+        n = len(array)
+    
+        # Build a maxheap.
+        for i in range(n//2 - 1, -1, -1):
+            self.heapify(array, n, i)
+    
+        # One by one extract elements
+        for i in range(n-1, 0, -1):
+            array[i], array[0] = array[0], array[i]  # swap
+            self.heapify(array, i, 0)
+    
