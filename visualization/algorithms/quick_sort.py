bd27752c0f2c0a00cda5bf0f41043f5b7db6f25a
Endri Dollani <44905521+endridollani@users.noreply.github.com>
2021-09-01 20:15:49 +0200

    Implemented quick sort function and partition function

--- visualization/algorithms/quick_sort.py
+++ visualization/algorithms/quick_sort.py
@@ -55,6 +55,56 @@ class QuickSort:
     def display(self, window):
         self.window = window
         self.quick_sort(self.array_of_numbers[0], self.array_of_numbers[len(
-            self.array_of_numbers-1)], self.array_of_numbers)
+            self.array_of_numbers) - 1], self.array_of_numbers)
         self.rectangles.draw_rectangles(window.get_window())
         self.rectangles.sort_finished(self.window.get_window(), 0.01)
+
+    # The main function that implements QuickSort
+    def quick_sort(self, start, end, array):
+        if(start < end):
+
+            # p is partitioning index, array[p]
+            # is at right place
+            p = self.partition(start, end, array)
+
+            # Sort elements before partition
+            # and after partition
+            self.quick_sort(start, p - 1, array)
+            self.quick_sort(p + 1, end, array)
+
+    # This Function handles sorting part of quick sort
+    # start and end points to first and last element of
+    # an array respectively
+
+    def partition(self, start, end, array):
+
+        # Initializing pivot's index to start
+        pivot_index = start
+        pivot = array[pivot_index]
+
+        # This loop runs till start pointer crosses
+        # end pointer, and when it does we swap the
+        # pivot with element on end pointer
+        while start < end:
+
+            # Increment the start pointer till it finds an
+            # element greater than  pivot
+            while start < len(array) and array[start] <= pivot:
+                start += 1
+
+            # Decrement the end pointer till it finds an
+            # element less than pivot
+            while array[end] > pivot:
+                end -= 1
+
+            # If start and end have not crossed each other,
+            # swap the numbers on start and end
+            if(start < end):
+                array[start], array[end] = array[end], array[start]
+
+        # Swap pivot element with element on end pointer.
+        # This puts pivot on its correct sorted place.
+        array[end], array[pivot_index] = array[pivot_index], array[end]
+
+        # Returning end pointer to divide the array into 2
+        return end
