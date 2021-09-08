7bdd8872454fd076f9ec746b323801f7f4c5364b
Endri Dollani <44905521+endridollani@users.noreply.github.com>
2021-09-08 23:10:33 +0200

    Implemented insertion sort

---
+++ visualization/algorithms/insertion_sort.py
@@ -0,0 +1,89 @@
+from pygame import draw
+from algorithms.rectangles import Rectangles
+import pygame
+from pygame import font
+import time
+
+font.init()
+font = pygame.font.SysFont('arial', 15)
+
+class InsertionSort(Rectangles):
+    def __init__(self, window, number_of_rectangles, delay):
+        super().__init__(window, number_of_rectangles, delay)
+        self.window = window
+        self.delay = delay
+        self.number_of_rectangles = number_of_rectangles
+        self.swaps = 0
+        self.swap_index = 0
+        self.swap_min_index = 0
+        self.array_accesses = 0
+
+    def sort(self):
+
+        # Traverse through 1 to len(arr)
+        for i in range(1, self.number_of_rectangles):
+            key = super().get_array_of_numbers()[i]
+
+            # Move elements of arr[0..i-1], that are
+            # greater than key, to one position ahead
+            # of their current position
+            j = i-1
+
+            while j >=0 and key < super().get_array_of_numbers()[j] :
+                    super().get_array_of_numbers()[j+1] = super().get_array_of_numbers()[j]
+                    time.sleep(self.delay)
+                    j -= 1
+            
+            super().get_array_of_numbers()[j+1] = key
+            self.draw(i,j)
+            
+
+        super().draw() 
+        super().draw_finishline()
+            
+
+    def set_defaults(self):
+        self.swaps = 0
+        self.swap_index = 0
+        self.swap_min_index = 0
+        self.array_accesses = 0
+   
+    def get_array_of_numbers(self):
+        return super().get_array_of_numbers()
+    
+    def draw(self,index_i,index_j):
+        self.window.set_background()
+        
+        self.information()
+        
+        for i in super().get_array_of_numbers():
+            val = i
+            pos = super().get_array_of_numbers().index(val)
+            rectangle = super().get_rectangle_at(pos,val)
+            
+            if pos == index_i:
+                pygame.draw.rect(self.window.get_display(),(0,255,0), rectangle)
+            elif pos == index_j+1 or pos == index_j:
+                pygame.draw.rect(self.window.get_display(),(255,0,0), rectangle)
+            else:
+                pygame.draw.rect(self.window.get_display(),(255,255,255), rectangle)
+
+        pygame.display.flip()
+    
+    def information(self):
+        algorithm = font.render(
+            "Selection Sort", True, (255, 255, 255), (0, 0, 0))
+        rectangles_drawn = font.render(
+            f'Rectangles: {len(super().get_array_of_numbers())}', True, (255, 255, 255), (0, 0, 0))
+        swaps = font.render(
+            f'Swaps: {self.swaps}', True, (255, 255, 255), (0, 0, 0))
+        array_accesses = font.render(
+            f'Array Accesses: {self.array_accesses}', True, (255, 255, 255), (0, 0, 0))
+        delays = font.render(
+            f'Delay {self.delay * 1000} ms', True, (255, 255, 255), (0, 0, 0))
+
+        self.window.get_display().blit(algorithm, (10, 10))
+        self.window.get_display().blit(rectangles_drawn, (10, 30))
+        self.window.get_display().blit(swaps, (10, 50))
+        self.window.get_display().blit(array_accesses, (10, 70))
+        self.window.get_display().blit(delays, (10, 90))
--- visualization/draw.py
+++ visualization/draw.py
@@ -6,6 +6,7 @@ from algorithms import quick_sort
 from algorithms import heap_sort
 from algorithms import merge_sort
 from algorithms import selection_sort
+from algorithms import insertion_sort
 
 class Draw(rectangles.Rectangles):
     def __init__(self,window,number_of_rectangles,delay_in_millisecondes):
@@ -20,6 +21,7 @@ class Draw(rectangles.Rectangles):
         self.hs = heap_sort.HeapSort(self.window,self.number_of_rectangles,self.delay_in_millisecondes)
         self.ms = merge_sort.MergeSort(self.window,self.number_of_rectangles,self.delay_in_millisecondes)
         self.ss = selection_sort.SelectionSort(self.window,self.number_of_rectangles,self.delay_in_millisecondes)
+        self.ins = insertion_sort.InsertionSort(self.window,self.number_of_rectangles,self.delay_in_millisecondes)
 
         self.is_required_for_sorting  = False
         self.sort_index = -1
@@ -43,6 +45,7 @@ class Draw(rectangles.Rectangles):
         self.hs.set_defaults()
         self.ms.set_defaults()
         self.ss.set_defaults()
+        self.ins.set_defaults()
         
     def sort_visualization(self):
         if self.sort_index == 0 and not self.array_sorted:
@@ -51,6 +54,9 @@ class Draw(rectangles.Rectangles):
         elif self.sort_index == 1 and not self.array_sorted:
             self.ss.sort()
             super().set_array_of_numbers(self.ss.get_array_of_numbers())
+        elif self.sort_index == 2 and not self.array_sorted:
+            self.ins.sort()
+            super().set_array_of_numbers(self.ins.get_array_of_numbers())
         elif self.sort_index == 3 and not self.array_sorted:
             self.ms.sort()
             super().set_array_of_numbers(self.ms.get_array_of_numbers())
@@ -71,6 +77,8 @@ class Draw(rectangles.Rectangles):
             self.bs.information()
         elif self.sort_index == 1:
             self.ss.information()
+        elif self.sort_index == 2:
+            self.ins.information()
         elif self.sort_index == 3:
             self.ms.information()
         elif self.sort_index == 4:
